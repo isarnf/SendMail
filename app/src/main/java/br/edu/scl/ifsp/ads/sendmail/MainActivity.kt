6479b257e93e09403307d11d51d9a79922b8889a
isarnf <isarnf@gmail.com>
2023-04-29 18:09:09 -0300

    implemented activity_main layout

---
+++ app/src/main/java/br/edu/scl/ifsp/ads/sendmail/MainActivity.kt
@@ -0,0 +1,15 @@
+package br.edu.scl.ifsp.ads.sendmail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.edu.scl.ifsp.ads.sendmail.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val amb: ActivityMainBinding by lazy{
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(amb.root)
+    }
+}
